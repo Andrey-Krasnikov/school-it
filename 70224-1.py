2fdb94e85bbd547a21f7a6ea8f6aa615a963c6e4
Andrey-Krasnikov <a8827n@gmail.com>
2024-02-28 12:36:05 +0200

    commit

---
+++ 70224-1.py
@@ -0,0 +1,17 @@
+count = int(input("Введіть к-кість чисел: "))
+list = []
+find = int(input("Введіть шукане у списку:"))
+res = 0
+
+for i in range(count):
+    num = int(input(f"Введіть число №{i+1}: "))
+    if num > 100 or num < 1:
+        print("Введено число, яке не відповідає умові")
+        break
+    list.append(num)
+
+    if num==find:
+        res+=1
+
+
+print(f"Мін.число: {min(list)}, Макс.число: {max(list)}, Число {find} зустрічається у списку {res} разів.")
